2cb09197ea0151a18a5ebc09e4527f77504ec729
guse95 <saveliy.gusev.05@list.ru>
2024-12-27 17:23:03 +0300

    Lab3 done

--- Lab3/client.c
+++ Lab3/client.c
@@ -14,45 +14,54 @@
 #define SEM_CHILD "/child_semaphore"
 
 int main(int argc, char **argv) {
+    // Проверка количества аргументов
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s output_file\n", argv[0]);
+        return EXIT_FAILURE;
+    }
 
+    // Открытие общей памяти
     int fd = shm_open(SHM_NAME, O_RDWR, 0666);
     if (fd == -1) {
-        const char msg[] = "error: failed to make shm_open\n";
-        write(STDERR_FILENO, msg, sizeof(msg));
-        exit(EXIT_FAILURE);
-    }
-    char *ptr = (char *)mmap(NULL, BUFSIZ, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-    if (ptr == MAP_FAILED) {
-        const char msg[] = "error: failed to make mmap\n";
-        write(STDERR_FILENO, msg, sizeof(msg));
-        close(fd);
-        exit(EXIT_FAILURE);
+        perror("shm_open");
+        return EXIT_FAILURE;
     }
 
+    // Отображение общей памяти в адресное пространство процесса
+    char *ptr = (char *)mmap(NULL, BUFSIZ, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    if (ptr == MAP_FAILED) {
+        perror("mmap");
+        close(fd);
+        return EXIT_FAILURE;
+    }
+
+    // Создание семафоров
     sem_t *sem_parent = sem_open(SEM_PARENT, O_CREAT, 0666, 0);
     sem_t *sem_child = sem_open(SEM_CHILD, O_CREAT, 0666, 0);
+
     if (sem_parent == SEM_FAILED || sem_child == SEM_FAILED) {
-        if (sem_parent == SEM_FAILED || sem_child == SEM_FAILED) {
-            const char msg[] = "error: failed to create semaphore\n";
-            write(STDERR_FILENO, msg, sizeof(msg));
-            close(fd);
-            exit(EXIT_FAILURE);
-        }
-        exit(EXIT_FAILURE);
+        perror("sem_open");
+        munmap(ptr, BUFSIZ);
+        close(fd);
+        return EXIT_FAILURE;
     }
 
     pid_t pid = getpid();
-    int32_t file = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC | O_APPEND, 0600);
+    int32_t file = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC, 0600);
+
     if (file == -1) {
-        const char msg[] = "error: failed to open requested file\n";
-        write(STDERR_FILENO, msg, sizeof(msg));
-        exit(EXIT_FAILURE);
+        perror("open");
+        sem_close(sem_parent);
+        sem_close(sem_child);
+        munmap(ptr, BUFSIZ);
+        close(fd);
+        return EXIT_FAILURE;
     }
 
     {
         char msg[128];
         int32_t len = snprintf(msg, sizeof(msg) - 1,
-                               "%d: Start typing lines of text. Press 'Ctrl-D' or 'Enter' with no input to exit\n", pid);
+                               "%d: Start typing lines of text. Press 'Ctrl-D' with no input to exit\n", pid);
         write(STDOUT_FILENO, msg, len);
     }
 
@@ -70,71 +79,40 @@ int main(int argc, char **argv) {
         }
 
         float sum = 0;
-        if (bytes < 0) {
-            abort();
-//            const char msg[] = "error: failed to read from stdin\n";
-//            write(STDERR_FILENO, msg, sizeof(msg));
-//            exit(EXIT_FAILURE);
-        } else if (buf[0] == '\0') {
-            break;
-        }
 
-        {
-            char msg[32];
-            int32_t len = snprintf(msg, sizeof(msg) - 1,
-                                   "Sum of your numbers: ");
+        // Обработка строки чисел
+        char *token = strtok(buf, " ");
+        while (token != NULL) {
+            char *endptr;
+            double num = strtod(token, &endptr);
 
-            int32_t written = write(file, msg, len);
-            if (written != len) {
-                const char msg[] = "error: failed to write to file\n";
-                write(STDERR_FILENO, msg, sizeof(msg));
-                exit(EXIT_FAILURE);
-            }
-        }
-
-        {
-            buf[bytes] = '\0';
-            int point_cnt = 0;
-            for (int i = 0; i < bytes - 1; ++i) {
-                if (isdigit(buf[i]) || (buf[i] == '.' && !point_cnt)) {
-                    if (buf[i] == '.') point_cnt++;
-                    continue;
-                }
-                if (buf[i] == ' ') {
-                    point_cnt = 0;
-                    buf[i] = '\0';
-                    continue;
-                }
-                const char msg[] = "error: value is not a number\n";
-                write(STDERR_FILENO, msg, sizeof(msg));
-                exit(EXIT_FAILURE);
-            }
-        }
-
-        {
-            char *ptr = buf;
-            float numb = 0;
-            sum += atof(ptr);
-            for(int i = 0; i < bytes - 1; ++i) {
-                if (buf[i] == '\0' && bytes > i + 1) {
-                    numb = atof(ptr + i + 1);
-                    sum += numb;
-                }
+            if (*endptr != '\0') {
+                fprintf(stderr, "error: value is not a number\n");
+                sem_post(sem_parent);
+                continue;
             }
 
-            size_t ansLen = snprintf(ans, sizeof(ans), "%.5f\n", sum);
-            int32_t written = write(file, ans, ansLen);
-            if (written != ansLen) {
-                const char msg[] = "error: failed to write to file\n";
-                write(STDERR_FILENO, msg, sizeof(msg));
-                exit(EXIT_FAILURE);
-            }
+            sum += num;
+            token = strtok(NULL, " ");
         }
+
+        // Запись суммы в файл
+        size_t ansLen = snprintf(ans, sizeof(ans), "%.5f\n", sum);
+        int32_t written = write(file, ans, ansLen);
+        if (written != ansLen) {
+            perror("write");
+            sem_post(sem_parent);
+            continue;
+        }
+
         sem_post(sem_parent);
     }
+
+    // Завершение записи в файл
     const char term = '\0';
     write(file, &term, sizeof(term));
 
+    // Освобождение ресурсов
     munmap(ptr, BUFSIZ);
     close(fd);
     sem_close(sem_parent);
--- Lab3/server.c
+++ Lab3/server.c
@@ -16,120 +16,88 @@
 static char CLIENT_PROGRAM_NAME[] = "client";
 
 int main(int argc, char **argv) {
-    printf("HUI");
     if (argc == 1) {
-        char msg[1024];
-        uint32_t len = snprintf(msg, sizeof(msg) - 1, "usage: %s filename\n", argv[0]);
-        write(STDERR_FILENO, msg, len);
+        fprintf(stderr, "Usage: %s filename\n", argv[0]);
         exit(EXIT_SUCCESS);
     }
 
     char progpath[1024];
-    {
-        ssize_t len = readlink("/proc/self/exe", progpath, sizeof(progpath) - 1);
-        if (len == -1) {
-            const char msg[] = "error: failed to read full program path\n";
-            write(STDERR_FILENO, msg, sizeof(msg));
-            exit(EXIT_FAILURE);
-        }
-
-        while (progpath[len] != '/')
-            --len;
-
-        progpath[len] = '\0';
+    ssize_t len = readlink("/proc/self/exe", progpath, sizeof(progpath) - 1);
+    if (len == -1) {
+        perror("readlink");
+        exit(EXIT_FAILURE);
     }
-    printf("HUI-1");
+
+    while (progpath[len] != '/') {
+        --len;
+    }
+
+    progpath[len] = '\0';
 
     int fd = shm_open(SHM_NAME, O_CREAT | O_RDWR, 0666);
     if (fd == -1) {
-        const char msg[] = "error: failed to make shm_open\n";
-        write(STDERR_FILENO, msg, sizeof(msg));
+        perror("shm_open");
         exit(EXIT_FAILURE);
     }
+
     if (ftruncate(fd, BUFSIZ) == -1) {
         perror("ftruncate");
-        exit(EXIT_FAILURE);
-    }
-    printf("HUI-2");
-    char *ptr = (char *)mmap(NULL, BUFSIZ, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-    if (ptr == MAP_FAILED) {
-        const char msg[] = "error: failed to make mmap\n";
-        write(STDERR_FILENO, msg, sizeof(msg));
         close(fd);
         exit(EXIT_FAILURE);
     }
-    printf("HUI-3");
+
+    char *ptr = (char *)mmap(NULL, BUFSIZ, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    if (ptr == MAP_FAILED) {
+        perror("mmap");
+        close(fd);
+        exit(EXIT_FAILURE);
+    }
+
     sem_t *sem_parent = sem_open(SEM_PARENT, O_CREAT, 0666, 0);
     sem_t *sem_child = sem_open(SEM_CHILD, O_CREAT, 0666, 0);
     if (sem_parent == SEM_FAILED || sem_child == SEM_FAILED) {
-        const char msg[] = "error: failed to create semaphore\n";
-        write(STDERR_FILENO, msg, sizeof(msg));
+        perror("sem_open");
         close(fd);
         exit(EXIT_FAILURE);
     }
-    printf("HUI");
-    char buf[BUFSIZ];
-    ssize_t bytes;
 
-    const pid_t child = fork();
+    pid_t child = fork();
+    if (child == -1) {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
 
     switch (child) {
-        case -1: {
-            const char msg[] = "error: failed to spawn new process\n";
-            write(STDERR_FILENO, msg, sizeof(msg));
-            exit(EXIT_FAILURE);
-        } break;
-
         case 0: {
-            printf("HUI2");
             pid_t pid = getpid();
+            printf("%d: I'm a child\n", pid);
 
+            char path[1024];
+            snprintf(path, sizeof(path) - 1, "%s/%s", progpath, CLIENT_PROGRAM_NAME);
 
-            {
-                char msg[64];
-                const int32_t length = snprintf(msg, sizeof(msg), "%d: I'm a child\n", pid);
-                write(STDOUT_FILENO, msg, length);
-            }
-
-            {
-                char path[1024];
-                snprintf(path, sizeof(path) - 1, "%s/%s", progpath, CLIENT_PROGRAM_NAME);
-
-                char *const args[] = {CLIENT_PROGRAM_NAME, argv[1], NULL};
-
-                int32_t status = execv(path, args);
-
-                if (status == -1) {
-                    const char msg[] = "error: failed to exec into new executable image\n";
-                    write(STDERR_FILENO, msg, sizeof(msg));
-                    exit(EXIT_FAILURE);
-                }
-            }
-        } break;
+            execl(path, CLIENT_PROGRAM_NAME, argv[1], NULL);
+            perror("execl");
+            exit(EXIT_FAILURE);
+        }
 
         default: {
-            printf("HUI3");
             pid_t pid = getpid();
-            {
-                char msg[64];
-                const int32_t length = snprintf(msg, sizeof(msg), "%d: I'm a parent, my child has PID %d\n", pid, child);
-                write(STDOUT_FILENO, msg, length);
-            }
+            printf("%d: I'm a parent, my child has PID %d\n", pid, child);
 
-            while ((bytes = read(STDIN_FILENO, buf, sizeof(buf) - 1))) {
+            char buf[BUFSIZ];
+            ssize_t bytes;
+            while ((bytes = read(STDIN_FILENO, buf, sizeof(buf) - 1)) > 0) {
                 buf[strcspn(buf, "\n")] = '\0';
-
                 strncpy(ptr, buf, BUFSIZ);
-
                 sem_post(sem_child);
-
                 sem_wait(sem_parent);
             }
 
             strncpy(ptr, "", BUFSIZ);
             sem_post(sem_child);
 
-            wait(NULL);
+            int status;
+            wait(&status);
 
             munmap(ptr, BUFSIZ);
             shm_unlink(SHM_NAME);
@@ -139,6 +107,7 @@ int main(int argc, char **argv) {
             sem_unlink(SEM_CHILD);
             close(fd);
         }
-        break;
     }
+
+    return 0;
 }
